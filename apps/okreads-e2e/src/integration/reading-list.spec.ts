5c5dcbfad7c5114522584d75209ffd50fca7a62c
Divya Devarapalli <divya.devarapalli.pkglobal@nisc.coop>
2023-07-03 20:02:53 +0530

    E2E
    E2E

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -11,4 +11,17 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  it('Then: I should set mark as read from reading list', () => {
+    cy.get('input[type="search"]').type('test');
+    cy.get('form').submit();
+    cy.get('[data-testing="want-to-read"]').first().click();
+    cy.wait(500);
+    cy.get('[data-testing="toggle-reading-list"]').first().click();
+    cy.get('[data-testing="mark-as-read"]').last().click();
+    cy.get('[data-testing="mark-as-read"]').should(
+      'contain.text',
+      'Finished'
+    );
+  });
 });
